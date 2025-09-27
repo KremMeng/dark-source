fb8c7a7ca758c204896d78051ce2102e2c76a955
KremMeng <1023345145@qq.com>
2025-09-27 10:50:56 +0800

    camera delay effect

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,9 +7,14 @@ public class CameraController : MonoBehaviour
     
     private GameObject playerHandle;
     private GameObject cameraHandle;
+    private GameObject _model;
+    private GameObject _camera;
 
     public float horizontalSpeed = 100.0f;
     public float verticalSpeed = 80.0f;
+    private Vector3 cameraDampVelocity;
+    public float cameraOffset = 0.05f;
+    
     [FormerlySerializedAs("eulerTemp")] public float eulerPitch = 20.0f;
     
     // Start is called before the first frame update
@@ -17,17 +22,24 @@ public class CameraController : MonoBehaviour
     {
         cameraHandle = transform.parent.gameObject;
         playerHandle = cameraHandle.transform.parent.gameObject;
+        _model = playerHandle.GetComponent<ActorController>().model;
+        if (Camera.main != null) _camera = Camera.main.gameObject;
     }
 
     // Update is called once per frame
-    void Update()
-    { 
+    void FixedUpdate()
+    {
+        //视角旋转
+        Vector3 eulerTemp = _model.transform.eulerAngles;
         //水平视角
-        playerHandle.transform.Rotate(Vector3.up, playerInput.lookRight * horizontalSpeed * Time.deltaTime);
-         
+        playerHandle.transform.Rotate(Vector3.up, playerInput.lookRight * horizontalSpeed * Time.fixedDeltaTime);
         //竖直视角
         eulerPitch = eulerPitch - playerInput.lookUp * verticalSpeed * Time.deltaTime;
         eulerPitch = Mathf.Clamp(eulerPitch, -40, 30);
         cameraHandle.transform.localEulerAngles = new Vector3(eulerPitch, 0, 0);
+        _model.transform.eulerAngles = eulerTemp;
+        //延迟相机
+        _camera.transform.eulerAngles = transform.eulerAngles;
+        _camera.transform.position = Vector3.SmoothDamp(_camera.transform.position, transform.position, ref cameraDampVelocity,cameraOffset);
     }
 }
