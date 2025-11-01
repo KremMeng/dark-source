9f2ca9750390e5a07b4df679c80757ba6d5e0ea4
KremMeng <1023345145@qq.com>
2025-11-02 02:22:24 +0800

    lockon direction

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -38,24 +38,34 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        //没有索敌的时候，正常水平竖直视角转换
+        if (lockTarget == null) {
+            Vector3 eulerTemp = _model.transform.eulerAngles; //保存当前朝向的角度
         
-        Vector3 eulerTemp = _model.transform.eulerAngles; //保存当前朝向的角度
+            //水平视角，由playerHandle方向定
+            playerHandle.transform.Rotate(Vector3.up, playerInput.lookRight * horizontalSpeed * Time.fixedDeltaTime);
+            //竖直视角，由cameraHandle方向定
+            eulerPitch = eulerPitch - playerInput.lookUp * verticalSpeed * Time.fixedDeltaTime;
+            eulerPitch = Mathf.Clamp(eulerPitch, -40, 30);
+            cameraHandle.transform.localEulerAngles = new Vector3(eulerPitch, 0, 0);//相对于父级角色的旋转,避免覆盖掉水平旋转
         
-        //水平视角，由playerHandle方向定
-        playerHandle.transform.Rotate(Vector3.up, playerInput.lookRight * horizontalSpeed * Time.fixedDeltaTime);
-        //竖直视角，由cameraHandle方向定
-        eulerPitch = eulerPitch - playerInput.lookUp * verticalSpeed * Time.fixedDeltaTime;
-        eulerPitch = Mathf.Clamp(eulerPitch, -40, 30);
-        cameraHandle.transform.localEulerAngles = new Vector3(eulerPitch, 0, 0);//相对于父级角色的旋转,避免覆盖掉水平旋转
-        
-        //让角色不跟着世界坐标转
-        _model.transform.eulerAngles = eulerTemp;//每一帧结尾，回到该帧初始角度
+            //让角色不跟着世界坐标转
+            _model.transform.eulerAngles = eulerTemp;//每一帧结尾，回到该帧初始角度
+        }
+        //索敌的时候，角色始终lookat目标敌人
+        else {
+            Vector3 dirPlayer2Enemy = lockTarget.transform.position - _model.transform.position;
+            dirPlayer2Enemy.y = 0;
+            playerHandle.transform.forward = dirPlayer2Enemy;
+            //_model.transform.forward = new Vector3(dirPlayer2Enemy.x,0,dirPlayer2Enemy.z);
+        }
         
         //延迟相机,主相机追cameraPos
         _camera.transform.position = Vector3.SmoothDamp(_camera.transform.position, transform.position, ref cameraDampVelocity, cameraOffset);
         //_camera.transform.eulerAngles = transform.eulerAngles;
         _camera.transform.LookAt(cameraHandle.transform); //固定看向后脖颈
     }
+    
     //索敌，解开索敌
     public void LockOrLockOn(){
         //用OverlapBox先计算碰撞体数组
@@ -68,8 +78,9 @@ public class CameraController : MonoBehaviour
             LayerMask.GetMask("Enemy"));
         foreach (var col in cols) {
             print(col.name);
+            lockTarget = col.gameObject;
+            break;
         }
-
         //索敌开关
     }
 }
