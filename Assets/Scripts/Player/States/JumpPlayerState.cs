f60facccb19a1c804784415e630f80bbd76fb1e7
KremMeng <1023345145@qq.com>
2026-01-30 05:49:40 +0800

    fix 4

--- Assets/Scripts/Player/States/JumpPlayerState.cs
+++ Assets/Scripts/Player/States/JumpPlayerState.cs
@@ -9,12 +9,15 @@ public class JumpPlayerState : PlayerState {
     } 
 
     protected override void OnStep(Player player){
-        // if (player.verticalVelocity.y <= 0) {
-        //     player.states.Change<IdlePlayerState>();
-        // }
-        player.verticalVelocity = new Vector3(0, 4.0f, 0);
-        //播放完切roll
-        player.ChangeOnAnimFinish(() =>  player.states.Change<RollPlayerState>());
+        if (player.horizontalVelocity.magnitude == 0) {
+            player.states.Change<IdlePlayerState>();
+        }
+        else {
+            player.verticalVelocity = new Vector3(0, 4.0f, 0);
+            //播放完切roll
+            player.ChangeOnAnimFinish(() =>  player.states.Change<RollPlayerState>());
+        }
+        
     }
 
     public override void OnContact(Player player, Collider other){
